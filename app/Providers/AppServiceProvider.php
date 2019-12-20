12af669a1cf2dbb53fda26c5b71ae29e2020ec9e
maidoan99 <maidoan2017@gmail.com>
2019-12-20 13:54:54 +0700

    add filter by category, author, ...

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 use App\Cart;
+use URL;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -24,13 +25,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        // view()->composer(['header'],function($view) {
-        //     if(Session('cart')) {
-        //         $oldCart = Session::get('cart');
-        //         $cart = new Cart($oldCart);
-        //         $view->with(['cart'=>Session::get('cart')]);
-        //     }
-        // });
+
+        URL::forceScheme('https');
         view()->composer('*',function($view) {
             $view->with([
                 'cart' => new Cart()               
